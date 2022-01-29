5ae7548f821213a29054263f071c4cb08cfd06cf
Richie Mugambi <richiemugambi4@gmail.com>
2022-01-29 19:31:54 +0300

    finished adding teniary operator to toggle between login and register

---
+++ client/src/components/Alert.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { useState } from 'react';
+
+const Alert = () => {
+    // const [showAleart, setshowAleart] = useState(false);
+
+    return <div className='alert alert-danger'>aleart goes here</div>;
+};
+
+export default Alert;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Logo, FormRow } from '../components';
+import { Logo, FormRow, Alert } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 
 const initialState = {
     isMember: true,
     name: '',
     email: '',
-    password: ''
+    password: '',
+    showAlert: true
 }
 
 const Register = () => {
@@ -21,14 +22,22 @@ const Register = () => {
         e.preventDefault()
         console.log(e.target);
     }
+    const toggleMember = () => {
+        setvalues({ ...values, isMember: !values.isMember })
+    }
     return (
         <Wrapper className='full-page'>
             <form className='form' onSubmit={handleSubmit}>
                 <Logo />
-                <FormRow type='text' name='name' value={values.name} handleChange={handleChange} />
+                <h3>{values.isMember ? "login" : "register"}</h3>
+                {values.showAlert && <Alert />}
+                {!values.isMember && (<FormRow type='text' name='name' value={values.name} handleChange={handleChange} />)}
                 <FormRow type='text' name='email' value={values.email} handleChange={handleChange} />
                 <FormRow type='password' name='password' value={values.password} handleChange={handleChange} />
                 <button type='submit' className='btn btn-block'>submit</button>
+                <p>
+                    {values.isMember ? "Not a member yet?" : "Already a member?"}
+                    <button type='button' onClick={toggleMember} className='member-btn'>{values.isMember ? "Register" : "Login"}</button></p>
             </form>
 
         </Wrapper>);
