c3a60004dcfe182dda51e82a60473a3688bffa8b
Richie Mugambi <richiemugambi4@gmail.com>
2022-02-18 15:27:54 +0300

    added validation error

--- controllers/authHandlerController.js
+++ controllers/authHandlerController.js
@@ -1,11 +1,15 @@
-const register=(req,res)=>{
-    res.send('register')
+import User from "../models/user.js"
+import { StatusCodes } from "http-status-codes"
+
+const register = async (req, res) => {
+    const user = await User.create(req.body)
+    res.status(StatusCodes.CREATED).json(user)
 }
-const login=(req,res)=>{
+const login = (req, res) => {
     res.send('login')
 }
-const updateUser=(req,res)=>{
+const updateUser = (req, res) => {
     res.send('updateUser')
 }
 
-export {register,login,updateUser}
+export { register, login, updateUser }
--- middleware/errorHandlermiddleware.js
+++ middleware/errorHandlermiddleware.js
@@ -1,4 +1,18 @@
+import { StatusCodes } from "http-status-codes"
+
 export const errorHandler = (error, req, res, next) => {
-    console.log(error);
-    return res.status(501).send('internal server error')
+    const defaultError = {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: "Sorry something went wrong try again later"
+    }
+    if (error.name === 'ValidationError') {
+        defaultError.statusCode = StatusCodes.BAD_REQUEST
+        defaultError.msg = error.message
+
+    }
+    // console.log(error);
+    // return res.status(defaultError.statusCode).json({ msg: defaultError.msg })
+    return res.status(defaultError.statusCode).json({ msg: error })
+
 }
+
