d53006a2761db07f876625c8478b44ba930faf54
Richie Mugambi <richiemugambi4@gmail.com>
2022-03-12 09:11:12 +0300

    added auth register

--- controllers/authHandlerController.js
+++ controllers/authHandlerController.js
@@ -14,10 +14,9 @@ const register = async (req, res) => {
     const user = await User.create({ name, email, password })
     const token = user.createjwt()
     res.status(StatusCodes.CREATED)
-    res.status(StatusCodes.CREATED).
-        json({ user: { name: user.name, email: user.email, location: user.location, lastname: user.lastname } })
-    //.json({user: {email: user.email,lastName: user.lastName,location: user.location,name: user.name,},token,location: user.location,})
-    //.json({ user: { name: user.name, lastname: user.lastname, email: user.email, location: user.location } }, token)
+    res.status(StatusCodes.CREATED)
+        .json({ user: { name: user.name, email: user.email, location: user.location, lastName: user.lastName }, token, location: user.location })
+
 }
 const login = (req, res) => {
     res.send('login')
